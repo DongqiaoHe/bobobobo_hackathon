2a9de177e17155966dc1d7d3f69b337035b78a5e
Amelia-Wu <wxySH@outlook.com>
2023-09-26 15:43:03 +1000

    add blog component

--- bobobobo_frontend/src/App.tsx
+++ bobobobo_frontend/src/App.tsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
-import Login from './component/login'
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import './App.css';
+import Login from './component/login';
+import Blog from './component/blog/Blog';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
-  )
+      <BrowserRouter>
+          <Routes>
+              <Route
+                  path="/"
+                  element={(
+                      <Login />
+                  )}
+              />
+              <Route
+                  path="/login"
+                  element={(
+                      <Login />
+                  )}
+              />
+              <Route
+                  path="/blog"
+                  element={(
+                      <Blog />
+                  )}
+              />
+          </Routes>
+      </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
