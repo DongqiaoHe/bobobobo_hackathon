5a4271e752d8d889c7df4f72b691700eb467bc20
Zheng YU <zyuu0045@gmail.com>
2023-09-26 17:11:08 +1000

    add the profile page to the App.tsx

--- bobobobo_frontend/src/App.tsx
+++ bobobobo_frontend/src/App.tsx
@@ -1,35 +1,22 @@
-import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import './App.css';
-import Login from './component/login/Login';
-import Blog from './component/blog/Blog';
-import SignUp from './component/signup/SignUp';
+import React from 'react'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import './App.css'
+import Login from './component/login/Login'
+import Blog from './component/blog/Blog'
+import SignUp from './component/signup/SignUp'
+import Profile from './component/Profile/Profile'
 
 function App() {
   return (
-      <BrowserRouter>
-          <Routes>
-              <Route
-                  path="/"
-                  element={(
-                      <Login />
-                  )}
-              />
-              <Route
-                  path="/register"
-                  element={(
-                      <SignUp />
-                  )}
-              />
-              <Route
-                  path="/blog"
-                  element={(
-                      <Blog />
-                  )}
-              />
-          </Routes>
-      </BrowserRouter>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<SignUp />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
