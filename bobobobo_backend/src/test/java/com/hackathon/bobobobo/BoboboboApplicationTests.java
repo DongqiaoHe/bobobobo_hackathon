bd2a23ae6ce95505dbb9b7bfb13045d67c4eb094
DongqiaoHe <hdq555555@gmail.com>
2023-09-26 10:02:57 +1000

    first

---
+++ bobobobo_backend/src/test/java/com/hackathon/bobobobo/BoboboboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.bobobobo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoboboboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
