8057b798a97566a2379fed5c02f06340ad5256ee
Zheng YU <zyuu0045@gmail.com>
2023-09-26 14:40:45 +1000

    changes to pictures

--- bobobobo_frontend/src/App.tsx
+++ bobobobo_frontend/src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Login from './component/login';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Login from './component/login'
 
 function App() {
   return (
     <div className="App">
       <Login />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
