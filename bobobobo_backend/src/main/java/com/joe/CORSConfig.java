f66285fd59dd5f123ae70cd6180e71d982df9405
DongqiaoHe <hdq555555@gmail.com>
2023-09-27 14:14:27 +1000

    add patch cors

--- bobobobo_backend/src/main/java/com/joe/CORSConfig.java
+++ bobobobo_backend/src/main/java/com/joe/CORSConfig.java
@@ -16,7 +16,7 @@ public class CORSConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS" , "PATCH");
             }
         };
     }
